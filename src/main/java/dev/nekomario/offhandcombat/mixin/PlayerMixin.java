219a08932138cc425b67332c114e0ac26674f2a7
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-07-26 16:02:21 +0900

    Use inherited LivingEntity attack strength field

--- src/main/java/dev/nekomario/offhandcombat/mixin/PlayerMixin.java
+++ src/main/java/dev/nekomario/offhandcombat/mixin/PlayerMixin.java
@@ -22,7 +22,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -31,9 +30,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @SuppressWarnings("unchecked")
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntity implements OffhandAttackAccess {
-    @Shadow
-    private int attackStrengthTicker;
-
     protected PlayerMixin(EntityType<? extends LivingEntity> type, Level level) {
         super(type, level);
     }
