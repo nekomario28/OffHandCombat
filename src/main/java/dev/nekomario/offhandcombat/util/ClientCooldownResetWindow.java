34528de9135f00f3be7c8e569ffa7b52150efffa
Yuu <206304251+nekomario28@users.noreply.github.com>
2026-08-05 01:25:09 +0900

    Isolate client cooldown reset window

---
+++ src/main/java/dev/nekomario/offhandcombat/util/ClientCooldownResetWindow.java
@@ -0,0 +1,17 @@
+package dev.nekomario.offhandcombat.util;
+
+public final class ClientCooldownResetWindow {
+    private long lastSequence;
+
+    public boolean mark(long sequence) {
+        if (sequence <= 0L || sequence == lastSequence) {
+            return false;
+        }
+        lastSequence = sequence;
+        return true;
+    }
+
+    public void copyFrom(ClientCooldownResetWindow source) {
+        lastSequence = source.lastSequence;
+    }
+}
